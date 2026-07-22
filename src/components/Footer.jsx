1e411db85f72d0025e41cb8f29a7d7d04d59d4c7
Netsai <hellonetsai@gmail.com>
2026-07-22 13:18:00 +0200

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import "./Footer.css";
+
+
+function Footer(){
+
+    return (
+
+        <footer className="footer">
+
+            <p>
+                © {new Date().getFullYear()} Netsai Chiyaka.
+                Built with React 🚀
+            </p>
+
+        </footer>
+
+    );
+
+}
+
+
+export default Footer;
