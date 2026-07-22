0117f83d7eb630f925e41c3fa5c8ee9f1bcf3a84
Netsai <hellonetsai@gmail.com>
2026-07-22 12:09:01 +0200

    Create portfolio landing page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <h2>Netsai.</h2>
+      </div>
+
+      <ul className="nav-links">
+        <li><a href="#about">About</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <button className="hire-btn">Hire Me</button>
+    </nav>
+  );
+}
+
+export default Navbar;
