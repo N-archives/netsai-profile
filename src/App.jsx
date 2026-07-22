fbbc03dff05e51dba6ef7d3e52b75a4fda9db60d
Netsai <hellonetsai@gmail.com>
2026-07-22 12:59:23 +0200

    Experience summary

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
-import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
 
 import "./index.css";
 
@@ -12,15 +13,18 @@ function App(){
  return (
 
   <>
+
     <Navbar />
 
     <Hero />
 
     <About />
 
+    <Skills />
+
     <Projects />
 
-     <Skills />
+    <Experience />
 
   </>
 
